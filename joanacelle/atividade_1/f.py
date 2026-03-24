f1427f92da58b1c305977e21b3396d70b2bb15aa
Olig Dev <beatsolig@gmail.com>
2026-03-24 07:19:07 -0300

    Delete joanacelle directory

--- joanacelle/atividade_1/f.py
+++
@@ -1,4 +0,0 @@
-#Algoritmo para converter temperatura de Celsius para Fahrenheit
-celsius = float(input("Digite a temperatura em Celsius: "))
-fahrenheit = (celsius * 1.8) + 32
-print("A temperatura em Fahrenheit é: {:.2f}".format(fahrenheit))
